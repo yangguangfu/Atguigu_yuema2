5de5c9b60a9a4cc61c8179422968d809fdde5f66
hellobenji <863360109@qq.com>
2016-05-20 16:52:07 +0800

    Update MainActivity.java

--- app/src/main/java/com/atguigu/atguigu_yuema/MainActivity.java
+++ app/src/main/java/com/atguigu/atguigu_yuema/MainActivity.java
@@ -11,7 +11,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("约炮功能已经开发完成，已经可以正常约了");
         System.out.println("约炮功能已经开发完成，已经加上定位功能了，可以找到附近的妹子了");
-        System.out.println("版本更新了");      
+        System.out.println("版本更新了");    
+        System.out.println("呵呵。。。你们被捕了。。涉黄");      
 		//版本更新了   
 		}
 }
