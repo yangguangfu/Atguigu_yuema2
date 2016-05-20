c067923bfd17d73bbea8da1fcad09f33695ea3da
yangguangfu <trygf521@126.com>
2016-05-20 16:31:42 +0800

    提交约炮软件-好玩的

---
+++ app/src/main/java/com/atguigu/atguigu_yuema/MainActivity.java
@@ -0,0 +1,14 @@
+package com.atguigu.atguigu_yuema;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("约炮功能已经开发完成，已经可以正常约了");
+    }
+}
