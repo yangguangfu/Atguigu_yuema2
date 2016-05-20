e0d268b1d198c32114e0d472f26678b1579a56bf
yangguangfu <trygf521@126.com>
2016-05-20 16:42:34 +0800

    版本更新了2222

--- app/src/main/java/com/atguigu/atguigu_yuema/MainActivity.java
+++ app/src/main/java/com/atguigu/atguigu_yuema/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("约炮功能已经开发完成，已经可以正常约了");
         System.out.println("约炮功能已经开发完成，已经加上定位功能了，可以找到附近的妹子了");
         System.out.println("版本更新了");
+        System.out.println("版本更新了");
         //版本更新了
     }
 }
